f7c733205ac18f3ea5c293eba4d56caa503acf3e
Ken Lee <ken.lee@nokia.com>
2016-11-24 18:00:44 +0800

    create race functionality

--- src/test/features/create_race.feature
+++ src/test/features/create_race.feature
@@ -1,5 +1,16 @@
 Feature: Create Race
 
-  Scenario: Create new race
-    When  I try to create a new race
-    Then  I must see the new race
+  Scenario: Name the Race
+    Given I have a race with no name
+    When  I try to name the race
+    Then  I must see the race using the new name
+
+  Scenario: Add first player
+    Given I have no players
+    When  I try to add a new player
+    Then  I must see the new player added
+
+#  Scenario: Add another player
+#    Given I have a player
+#    When  I try to add another player
+#    Then  I must see both players in the list
--- src/test/java/cucumber/stepdefs/CreateRaceStepdefs.java
+++ src/test/java/cucumber/stepdefs/CreateRaceStepdefs.java
@@ -1,16 +1,48 @@
 package cucumber.stepdefs;
 
-import cucumber.api.PendingException;
 import cucumber.api.java8.En;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+import static cucumber.stepdefs.support.Url.url;
+import static cucumber.stepdefs.support.WebDrivers.getDriver;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
 public class CreateRaceStepdefs implements En {
     public CreateRaceStepdefs() {
-        When("^I try to create a new race$", () -> {
-            throw new PendingException();
+        Given("^I have a race with no name", () -> {
+            getDriver().get(url("/createRace"));
         });
 
-        Then("^I must see the new race$", () -> {
-           throw new PendingException();
+        When("^I try to name the race$", () -> {
+            WebElement elem = getDriver().findElement(By.cssSelector("#race-name"));
+            elem.sendKeys("My Race");
+        });
+
+        Then("^I must see the race using the new name$", () -> {
+            WebElement elem = getDriver().findElement(By.cssSelector("#race-name"));
+
+            assertThat(elem.getAttribute("value"), is("My Race"));
+        });
+
+        Given("^I have no players", () -> {
+            getDriver().get(url("/createRace"));
+        });
+
+        When("^I try to add a new player$", () -> {
+            WebElement newPlayer = getDriver().findElement(By.cssSelector("#new-player"));
+            newPlayer.sendKeys("Player 1");
+            WebElement addPlayer = getDriver().findElement(By.cssSelector("#add-player"));
+            addPlayer.click();
+        });
+
+        Then("^I must see the new player added$", () -> {
+            List<WebElement> playerList = getDriver().findElements(By.cssSelector("#player-list li"));
+
+            assertThat(playerList.size(), is(1));
         });
     }
 }
